6d7e80976c85f0e179c11998e39c968a46ececca
Chakaka <bis19-skalola@poly.ac.mw>
2024-02-03 11:05:06 +0200

    everything updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,89 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-  <div class=" ">
+<div class="container pb-4 bg-white shadow-sm  " style="margin-top: -25px;" >
+  <div class=" pt-4 shadow-sm">
+    <h4 class="text-success  text-center about-h1 ">ABOUT US</h4>
+    <p>At Hedman Nurseries, we take pride in nurturing nature and providing top-quality pine 
+      and spruce seedlings to create a greener, more vibrant world. With a legacy spanning over 5 years,
+       we have established ourselves as a trusted name in the horticultural industry.Our Story
+        Founded in [Year], Hedman Nurseries began as a humble family venture, motivated by a 
+        deep-seated passion for nurturing the wonders of nature. Over the years, our commitment to excellence, 
+        sustainable practices,
+        and customer satisfaction has allowed us to blossom into a leading supplier of pine
+        and spruce seedlings.
+      </p>
+
+
+  </div>
+  <div class="shadow-sm">
+    <h4 class="text-success text-center about-h">Quality Assurance</h4>
+    <p>
+      
+      Our seedlings are carefully nurtured from the finest genetic stock,
+       ensuring robust growth and vitality. We employ rigorous quality
+      control measures at every stage of production to deliver seedlings that
+        exceed industry standards.
+    </p>
+
+  </div>
+  <div class="shadow-sm">
+    <h4 class="text-success text-center about-h">Our Products</h4>
+    <p>
+        
+        Explore our diverse range of pine and spruce seedlings, 
+        carefully curated to meet the needs of various environments
+        and landscaping projects. Our extensive collection reflects our dedication to 
+        providing options that thrive in different climates and soil conditions.
+    </p>
+
+  </div>
+  <div >
+    <div class="row">
+      <div class="col-md">
+        <div class="card text-center about-card">
+          <div class="card-body">
+            <h4 class=" fa fa-book text-success "></h4>
+            <h4 class="card-title text-success about-card-h"  >Our Mission</h4>
+            <p class="card-text">At the heart of Hedman Nurseries lies a mission to promote environmental 
+              sustainability by offering premium-quality seedlings. We firmly believe in the transformative 
+              power of trees to positively impact the ecosystem and contribute to a greener, more sustainable
+              future. Through responsible cultivation and distribution,
+              we aim to be a catalyst for positive change in our environment.</p>
+            
+          </div>
+        </div>
+      </div>
+        <div class="col-md">
+           <div class="card text-center about-card">
+          <div class="card-body">
+            <h4 class=" fa fa-globe text-success about-card"></h4>
+            <h4 class="card-title text-success about-card-h"  >Our Vision</h4>
+            <p class="card-text">Hedman Nurseries envisions a world where every individual, community, and 
+              ecosystem thrives through the power of greenery. We strive to be at the forefront of 
+              fostering environmental consciousness and creating a lasting impact on the planet. 
+              Our vision is to inspire a global community to embrace the beauty of
+               nature and recognize the vital role that trees play in shaping a sustainable future.</p>
+            
+          </div>
+            </div>
+        </div>
+        <div class="col-md">
+          <div class="card text-center about-card">
+          <div class="card-body">
+            <h4 class=" fa fa-pen text-success"></h4>
+            <h4 class="card-title text-success about-card-h"  >Achievements</h4>
+            <p class="card-text">As of now Hedman Nurseries proudly achieved a significant milestone
+               by delivering 6 000 000 pine,blughum and other types of tree nursaries to different customers across Malawi. This initiative was part
+                of our commitment to global environmental responsibility, 
+              aiming to enhance green spaces and contribute to sustainable ecosystems in all regions of Malawi  </p>
+            
+          </div>
+        </div>
+ 
+    
+</div>
+    
+
 
   </div>
 </div>           
